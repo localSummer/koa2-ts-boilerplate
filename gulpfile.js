a686df4187d012ae860fd11f9caae19c568bc343
wangxingwen <wangxingwen@liepin.com>
2020-07-03 19:40:51 +0800

    feat:增加gulp打包配置

---
+++ gulpfile.js
@@ -0,0 +1,40 @@
+/* eslint-disable @typescript-eslint/no-var-requires */
+const gulp = require('gulp');
+const ts = require('gulp-typescript');
+const clean = require('gulp-clean');
+const uglify = require('gulp-uglify-es').default;
+const { series, watch } = require('gulp');
+const tsProject = ts.createProject('tsconfig.json');
+
+function cleanDist(cb) {
+  gulp.src('dist').pipe(clean());
+  cb();
+}
+
+function build(cb) {
+  tsProject
+    .src()
+    .pipe(tsProject())
+    .pipe(uglify())
+    .pipe(gulp.dest('dist'));
+  cb();
+}
+
+function dev() {
+  const compile = () => {
+    tsProject
+      .src()
+      .pipe(tsProject())
+      .pipe(gulp.dest('dist'));
+  };
+  compile();
+  watch(['src/**/*.ts'], function(cb) {
+    // body omitted
+    compile();
+    cb();
+  });
+}
+
+exports.dev = dev;
+
+exports.build = series(cleanDist, build);
